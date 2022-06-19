010249cd1d4939da4b78978c8d55c7cd96b00b85
Elton Marku <eltonmarku@gmail.com>
2022-06-19 13:47:40 +0200

    fix print

--- render.ts
+++ render.ts
@@ -23,8 +23,7 @@ export const render = async (
       "./templates/layout.eta",
       import.meta.url,
     );
-    const css = await getFileContent("./css/style.css", import.meta.url);
-
+    const mainCss = await getFileContent("./css/main.css", import.meta.url);
     const awardTemplateName = "awards";
     eta.templates.define(
       awardTemplateName,
@@ -168,11 +167,44 @@ export const render = async (
       ),
     );
 
+    const coverTemplate = "cover";
+    eta.templates.define(
+      coverTemplate,
+      eta.compile(
+        await getFileContent(
+          "./templates/cover.eta",
+          import.meta.url,
+        ),
+      ),
+    );
+
+    const tocTemplate = "toc";
+    eta.templates.define(
+      tocTemplate,
+      eta.compile(
+        await getFileContent(
+          "./templates/toc.eta",
+          import.meta.url,
+        ),
+      ),
+    );
+
     const dateRangeTemplateName = "date-range";
     eta.templates.define(
       dateRangeTemplateName,
       eta.compile(
         await getWidgetDateRangeFilePath(),
+      ),
+    );
+
+    const ratingTemplateName = "skill-rating";
+    eta.templates.define(
+      ratingTemplateName,
+      eta.compile(
+        await getFileContent(
+          "./templates/widgets/rating.eta",
+          import.meta.url,
+        ),
       ),
     );
 
@@ -213,6 +245,14 @@ export const render = async (
       listTemplateName,
       eta.compile(
         await getWidgetListFilePath(),
+      ),
+    );
+
+    const skillListTemplateName = "skillList";
+    eta.templates.define(
+      skillListTemplateName,
+      eta.compile(
+        await getWidgetSkillListFilePath(),
       ),
     );
 
@@ -279,7 +319,7 @@ export const render = async (
       orderedMap.set(resumeCategory, map.get(resumeCategory)!);
     });
     const result = await eta.render(layout, {
-      css: css,
+      mainCss: mainCss,
       widgetCss: widgetCss,
       resume: resume,
       type: type,
